1c0806f7a3ae076e24d2cbce3da86cd4b039d529
Nirvous19 <90237821+Nirvous19@users.noreply.github.com>
2024-06-11 13:39:26 +0200

    first project-structure and files

---
+++ php/config.example.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host' => 'YOUR_HOST',
+    'db'   => 'YOUR_DB',
+    'user' => 'YOUR_USER',
+    'pass' => 'YOUR_PASS',
+    'port' => 'YOUR_PORT',
+    'dsn'  => 'mysql:host=YOUR_HOST;dbname=YOUR_DB;port=YOUR_PORT;',
+];
+?>
